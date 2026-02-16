d3a6d2a3d46c91fbf16b8aa677b6a92e3e5bb024
BOSSe <BOSSe@danielnylander.se>
2026-02-16 16:01:18 +0100

    Bump version to 0.2.5

--- src/langpack_inspector/__init__.py
+++ src/langpack_inspector/__init__.py
@@ -1,4 +1,4 @@
 """Language Pack Inspector - GTK4/Adwaita app for inspecting Ubuntu language packs."""
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __app_id__ = "se.danielnylander.LangpackInspector"
